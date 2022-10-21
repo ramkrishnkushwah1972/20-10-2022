870012bca0bd0a7753d9191204073114f2d4a67c
Ramkrishna Kushwah <105698577+ramkrishnkushwah1972@users.noreply.github.com>
2022-10-21 14:50:27 +0530

    Update CheckFileExtensionClass.java

--- CheckFileExtensionClass.java
+++ CheckFileExtensionClass.java
@@ -65,17 +65,7 @@ public class CheckFileExtensionClass {
 		if(fileName.isEmpty()) {
 			throw new EmptyException("This is empty String");
 		}
-		
-		String file="";
-		//This loop used for get (.java) string from filename and store file variable 
-		//if you don't want to use for loop so you can use endsWith() method.
-		for(int i=fileName.length()-1; i>=fileName.length()-5; i--)
-		{
-			if(i>0)
-			file = fileName.charAt(i) + file;
-			
-		}
-		if(file.equals(".java"))
+		if(file.endsWith(".java"))
 			return 1;
 		
 		return 0;
